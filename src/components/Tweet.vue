21797d0bb3bd90872dbbb40b9a68f228a78b0604
BerengerH <berenger.humeau@yahoo.fr>
2022-08-06 13:38:47 +0200

    Iteration 1 and 9 were completed with first commit - Iteration 2 completed

---
+++ src/components/Tweet.vue
@@ -0,0 +1,46 @@
+<template>
+  <div className="tweet">
+    <img
+      :src= "tweet.user.image"
+      className="profile"
+      alt="profile"
+    />
+    <div className="body">
+      <div className="top">
+        <span className="user">
+          <span className="name">{{tweet.user.name}}</span>
+          <span className="handle">{{tweet.user.handle}}</span>
+        </span>
+        <span className="timestamp">{{tweet.timestamp}}</span>
+      </div>
+      <p className="message">
+          {{tweet.message}}
+      </p>
+      <div className="actions">
+        <!-- Font Awesome icons -->
+        <i class="far fa-comment"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+        <i class="fas fa-share"></i>
+      </div>
+    </div>
+    <i class="fas fa-ellipsis-h"></i>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Tweet",
+  props: {
+    'tweet': Object,
+  }
+  
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
